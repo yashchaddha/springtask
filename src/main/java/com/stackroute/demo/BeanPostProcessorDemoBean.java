ce00e1fe05bf6f8cc179b78d200bd0246f60c56e
yashchaddha <yash.chaddha1997@gmail.com>
2019-03-05 13:09:22 +0530

    Commit 3

---
+++ src/main/java/com/stackroute/demo/BeanPostProcessorDemoBean.java
@@ -0,0 +1,17 @@
+package com.stackroute.demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization() is called");
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization() is called");
+        return bean;
+    }
+}
